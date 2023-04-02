7dbc2ad74735370522ad43312fa997137dd527ba
Jesse Luoto <jesse.luoto@gmail.com>
2023-04-02 23:12:35 +0300

    Fix ERR_OSSL_UNSUPPORTED
    https://stackoverflow.com/a/74668003/2697279

--- src/env.ts
+++ src/env.ts
@@ -5,6 +5,8 @@ export const DATABASE_FILENAME =
   process.env.DATABASE_FILENAME ?? "./database.db";
 export const PORT = process.env.PORT ?? 3000;
 export const PUBLIC_KEY =
-  process.env.PUBLIC_KEY ?? fs.readFileSync("./public.pem").toString();
+  process.env.PUBLIC_KEY?.split(String.raw`\n`).join("\n") ??
+  fs.readFileSync("./public.pem").toString();
 export const PRIVATE_KEY =
-  process.env.PRIVATE_KEY ?? fs.readFileSync("./private.pem").toString();
+  process.env.PRIVATE_KEY?.split(String.raw`\n`).join("\n") ??
+  fs.readFileSync("./private.pem").toString();
