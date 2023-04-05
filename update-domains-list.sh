7d4f0a830176e863fdf1f108e6295cb5c25a528a
Jesse Luoto <jesse.luoto@gmail.com>
2023-04-05 22:52:19 +0300

    Remove microsoft and google from tlds
    Will cause more harm than good for now

--- update-domains-list.sh
+++ update-domains-list.sh
@@ -2,8 +2,10 @@
 
 # Update the list of domains from the public suffix list
 curl --silent https://publicsuffix.org/list/public_suffix_list.dat |
-    grep -v '^//' | # Remove comments
-    grep -v '^$'  | # Remove empty lines
-    grep -v '\.'  | # Remove lines with dots
-    sort          | # Remove duplicates
+    grep -v '^//'         | # Remove comments
+    grep -v '^$'          | # Remove empty lines
+    grep -v '\.'          | # Remove lines with dots
+    grep -v '^microsoft$' | # Remove Microsoft's tld (remove after you implement the many-tlds option)
+    grep -v '^google$'    | # Remove Microsoft's tld (remove after you implement the many-tlds option)
+    sort                  | # Remove duplicates
     uniq > tlds.dat
